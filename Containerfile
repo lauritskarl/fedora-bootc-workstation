60af28add5880535cfd300a48fd15091a80e8414
Karl Hans Laurits <karl@laurits.dev>
2025-06-05 03:55:05 +0300

    Add necessary package groups and iso installer script

--- Containerfile
+++ Containerfile
@@ -2,6 +2,8 @@ FROM quay.io/fedora/fedora-bootc:latest
 ADD etc etc
 RUN dnf install -y \
   @workstation-product \
+  @gnome-desktop \
+  @container-management \
   helix \
   gh && \
   dnf clean all
---
+++ create-iso.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+sudo podman run \
+    --rm \
+    -it \
+    --privileged \
+    --pull=newer \
+    --security-opt label=type:unconfined_t \
+    -v /var/lib/containers/storage:/var/lib/containers/storage \
+    -v $(pwd)/config.toml:/config.toml \
+    -v $(pwd)/output:/output \
+    quay.io/centos-bootc/bootc-image-builder:latest \
+    --type iso \
+    --config /config.toml \
+  ghcr.io/lauritskarl/fedora-bootc-workstation:latest
